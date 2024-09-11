c54d1996ff26aa3136c233caa621441173a8a0ad
Gurpreet Singh <108651530+GurpreetSingh-21@users.noreply.github.com>
2024-09-11 17:24:54 -0400

    Add files via upload

---
+++ 1. Two Sum/leetcode1.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> num_map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (num_map.containsKey(complement)) {
+                return new int[] { num_map.get(complement), i };
+            }
+            num_map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
