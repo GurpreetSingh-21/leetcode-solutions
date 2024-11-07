511f5d819d8cb208f3ffc2cf9cf566c17a4cf745
Singh Technology <harryghotra195@gmail.com>
2024-11-07 15:14:58 -0500

    Palindrome leetcode

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+class Palindrome {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int pali = 0;
+        while(x > pali) {
+            pali = pali * 10 + x % 10;
+            x = x / 10;
+        }
+        return (x == pali || x == pali / 10);
+    }
+
+    public static void main(String[] args) {
+        Palindrome sum = new Palindrome(); // Change Solution to Palindrome
+        int num = 121;
+        System.out.println(sum.isPalindrome(num));
+    }
+}
